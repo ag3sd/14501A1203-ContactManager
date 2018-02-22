7de7ed1d925fd2dba10afc5f3b28069bf86414eb
ag3sd <30552460+ag3sd@users.noreply.github.com>
2018-02-22 14:09:30 +0530

    Add files via upload

--- contacts.sql
+++ contacts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2018 at 07:38 PM
+-- Generation Time: Feb 22, 2018 at 09:36 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -39,9 +39,19 @@ CREATE TABLE `cinf` (
 --
 
 INSERT INTO `cinf` (`name`, `phone`, `email`, `gid`, `owner`) VALUES
+('ahmed', '2345678', 'sbdciasiksnc', 2, 'sai@inmar.com'),
+('asncksnl', '67890', 'hjnk m.', 0, 'sai@inmar.com'),
+('dhe', 'undefined', 'undefined', 0, 'sai@inmar.com'),
+('funny', 'undefined', 'undefined', 0, 'sai@inmar.com'),
+('jash', '67865', 'ghjjv', 2, 'sandy@inmar.com'),
+('jkslkn', 'undefined', 'undefined', 0, 'sai@inmar.com'),
+('johny', '456798765', 'johny@mail.com', 0, 'sai@inmar.com'),
+('kushi', 'undefined', 'undefined', 0, 'sai@inmar.com'),
+('p', 'undefined', 'undefined', 0, 'sai@inmar.com'),
 ('pavan', '4567890', 'pavan@mail.com', 2, 'sai@inmar.com'),
-('asncksnl', '67890', 'hjnk m.', 3, 'sai@inmar.com'),
-('ahmed', '2345678', 'sbdciasiksnc', 0, 'sai@inmar.com');
+('s', 'i', 'w', 7, 'sai@inmar.com'),
+('sameer', '6789009876', 'sam@mail.com', 2, 'sai@inmar.com'),
+('student', '9999999999', 'student@mail.com', 1, 'sandy@inmar.com');
 
 -- --------------------------------------------------------
 
@@ -60,9 +70,17 @@ CREATE TABLE `ginf` (
 --
 
 INSERT INTO `ginf` (`owner`, `gid`, `gname`) VALUES
-('sai@inmar.com', 2, 'pvpsit2'),
+('sai@inmar.com', 2, 'pvpsit'),
 ('sai@inmar.com', 3, 'notnew'),
-('sai@inmar.com', 4, 'test');
+('sai@inmar.com', 4, 'test'),
+('sai@inmar.com', 3, 'new Group'),
+('sai@inmar.com', 4, 'whats new'),
+('sai@inmar.com', 5, 'final'),
+('sai@inmar.com', 6, 'fiction in'),
+('sai@inmar.com', 7, 'something is wrong'),
+('sandy@inmar.com', 1, 'collage'),
+('sandy@inmar.com', 2, 'class'),
+('sandy@inmar.com', 4, 'room');
 
 -- --------------------------------------------------------
 
@@ -86,8 +104,8 @@ CREATE TABLE `inmareg` (
 --
 
 INSERT INTO `inmareg` (`email`, `fname`, `lname`, `psw`, `aadhar`, `phone`, `gender`, `gid`) VALUES
-('sai@inmar.com', 'srinivas', 'deekshith', 'satyakrishna.A1', '123456785666', '1122234445', 'Male', 3),
-('sandy@inmar.com', 'sandeep', 'dunnala', 'sandy.A1', '23456781234567', '23456789078', 'Male', 0);
+('sai@inmar.com', 'srinivas', 'deekshith', 'satyakrishna.A1', '123456785666', '1122234445', 'Male', 7),
+('sandy@inmar.com', 'sandeep', 'dunnala', 'sandy.A1', '23456781234567', '23456789078', 'Male', 4);
 
 --
 -- Indexes for dumped tables
